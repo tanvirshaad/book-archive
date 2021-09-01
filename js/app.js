791f67bd7eb9b27cb47c4cf93dd33ed7b3eb303a
tanvirshaad <tanvirshaad@gmail.com>
2021-09-02 00:00:44 +0600

    getting input and fetching done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+const searchBook = () => {
+    const searchBox = document.getElementById('search-box');
+    const searchText = searchBox.value;
+    // console.log(searchText);
+    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+};
