0205f988343032fee1eabad53250571bd611dc5a
tanvirshaad <tanvirshaad@gmail.com>
2021-09-02 22:03:17 +0600

    error fixed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const displayMessage = document.getElementById('display-message');
-
+// getting searched text
 const searchBook = () => {
     const searchBox = document.getElementById('search-box');
     const searchText = searchBox.value;
@@ -11,35 +11,38 @@ const searchBook = () => {
         const url = `http://openlibrary.org/search.json?q=${searchText}`;
         fetch(url)
             .then((res) => res.json())
-            .then((data) => displayBooks(data))
-            .catch((error) => displayError(error));
+            .then((data) => displayBooks(data));
     }
 };
-const displayError = (error) => {
-    displayMessage.innerText = 'Something went wrong. Please try again later';
-};
+//displaying books and result
 const displayBooks = (data) => {
     const searchResult = document.getElementById('search-result');
-    const resultNum = document.getElementById('result-number');
-    resultNum.innerHTML = `${data.numFound} Result Found`;
-    searchResult.textContent = '';
-    const books = data.docs;
-    books.forEach((book) => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-            <div class="card">
-                <img src= "https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${book.title}</h5>
-                    <p class="card-text">
-                        <h5>Author Name: <span class="fst-italic fw-normal">${book.author_name}</span></h6>
-                        <h6>Publisher: <span class="fw-normal">${book.publisher[0]}</span></h6>
-                        <small class="text-muted">First Publish: ${book.first_publish_year}</small>
-                    </p>
+    if (data.numFound === 0) {
+        displayMessage.innerText = 'No result found';
+    } else {
+        //getting result number
+        const resultNum = document.getElementById('result-number');
+        resultNum.innerHTML = `${data.numFound} Result Found`;
+        //displaying books
+        searchResult.textContent = '';
+        const books = data.docs;
+        books.forEach((book) => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                <div class="card">
+                    <img src= "https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${book.title}</h5>
+                        <p class="card-text">
+                            <h5>Author Name: <span class="fst-italic fw-normal">${book.author_name}</span></h6>
+                            <h6>Publisher: <span class="fw-normal">${book.publisher[0]}</span></h6>
+                            <small class="text-muted">First Publish: ${book.first_publish_year}</small>
+                        </p>
+                    </div>
                 </div>
-            </div>
-        `;
-        searchResult.appendChild(div);
-    });
+            `;
+            searchResult.appendChild(div);
+        });
+    }
 };
